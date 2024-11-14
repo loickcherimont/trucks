efcbeed08cde712f79cab0f29ee3b79b75d39f03
loickcherimont <loickcherimont@gmail.com>
2024-11-14 23:15:57 +0100

    Implement routes for DELETE, CREATE(add) operations

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -15,8 +15,11 @@ func GetRoutes() *mux.Router {
 	router.HandleFunc("/logout", handlers.LogoutHandler)
 
 	// Routes with authentication
+	// Controlled by middlewares
 	router.HandleFunc("/admin", middlewares.CheckLogging(handlers.AdminHandler))
 	router.HandleFunc("/admin/trucks", middlewares.CheckLogging(handlers.TrucksHandler))
+	router.HandleFunc("/admin/trucks/delete/{id}", middlewares.CheckLogging(handlers.DeleteTruck))
+	router.HandleFunc("/admin/trucks/add", middlewares.CheckLogging(handlers.AddTruck))
 
 	return router
 }
